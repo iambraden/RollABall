cc5503c8a3ec76398e0ca10401c82c5c79426ed4
iambraden <iambradencollins@outlook.com>
2025-01-25 13:55:10 -0800

    Added Debug.log for user input

--- Assets/InputManager.cs
+++ Assets/InputManager.cs
@@ -17,18 +17,22 @@ public class InputManager : MonoBehaviour
 
         if (Input.GetKey(KeyCode.W)){
             inputVector += Vector2.up;
+            Debug.Log("W key is pressed");
             }
 
         if (Input.GetKey(KeyCode.S)){
             inputVector += Vector2.down;
+            Debug.Log("S key is pressed");
             }
 
         if (Input.GetKey(KeyCode.A)){
             inputVector += Vector2.left;
+            Debug.Log("A key is pressed");
             }
 
         if (Input.GetKey(KeyCode.D)){
             inputVector += Vector2.right;
+            Debug.Log("D key is pressed");
             }
 
         OnMove?.Invoke(inputVector);
